55d6c9d55a150ae10e06e9e67fea6c38258ab356
SmithaVerity <smitha@theveritycorp.com>
2022-09-10 23:55:06 +0530

    Initial commit

---
+++ inc/config/constants.php
@@ -0,0 +1,21 @@
+<?php
+	// Root url for the site
+	define('ROOT_URL', 'http://43.205.114.70/IMS');
+	
+	
+	// Database parameters
+	// Data source name
+	define('DSN', 'mysql:host=collegemgmtdb.cgiubsxpc6xe.ap-southeast-1.rds.amazonaws.com;dbname=shop_inventory');
+	
+	// Hostname
+	define('DB_HOST', 'collegemgmtdb.cgiubsxpc6xe.ap-southeast-1.rds.amazonaws.com');
+	
+	// DB user
+	define('DB_USER', 'admin');
+	
+	// DB password
+	define('DB_PASSWORD', 'admin123');
+	
+	// DB name
+	define('DB_NAME', 'shop_inventory');
+?>
